4998b5f938df6a3c0dafc8fcfd64a6d8878772e0
KEHANG <kehanghan@gmail.com>
2017-06-08 13:32:10 -0400

    add MonteCarloSimulator sub-class

--- afm/simulator.py
+++ afm/simulator.py
@@ -23,5 +23,36 @@ class Simulator(object):
 		self.fragment_reaction_list = fragment_rxns + pseudo_fragrxns
 		self.fragment_dict = fragment_dict
 
+class MonteCarloSimulator(Simulator):
 
+	########################
+	# Construction section #
+	########################
+	def __init__(self, 
+				chemkin_path, 
+				dictionary_path,
+				input_fragment_count_dict,
+				molecule_list,
+				volume, 
+				temperature):
+		super(MonteCarloSimulator, self).__init__(chemkin_path, 
+												dictionary_path)
+
+		self.initialize_fragment_counts(input_fragment_count_dict)
+
+		self.molecule_list = molecule_list
+		self.volume = volume # unit: m^3
+		self.temperature = temperature
+
+	def initialize_fragment_counts(self, input_fragment_count_dict):
+
+		# initialize the fragment_count
+		self.fragment_count_dict = dict(zip(self.fragment_dict.keys(), 
+											[0]*len(self.fragment_dict)))
+
+		for frag in self.fragment_count_dict:
+			if frag in input_fragment_count_dict:
+				self.fragment_count_dict[frag] = input_fragment_count_dict[frag]
+
+	
 
