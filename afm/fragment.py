295d33767d0717bb14abd0c25acaf6989da3712f
KEHANG <kehanghan@gmail.com>
2017-10-18 11:21:53 -0400

    refactor fragment class a bit

--- afm/fragment.py
+++ afm/fragment.py
@@ -4,13 +4,13 @@ from rmgpy.molecule.graph import Graph
 class Fragment(Graph):
 
 	def __init__(self,
-				label,
-				species_repr,
-				atoms=None):
+				label='',
+				species_repr=None,
+				vertices=None):
 		self.index = -1
 		self.label = label
 		self.species_repr = species_repr
-		Graph.__init__(self, atoms)
+		Graph.__init__(self, vertices)
 
 	def __str__(self):
 		"""
@@ -19,6 +19,7 @@ class Fragment(Graph):
 		if self.index == -1: return self.label
 		else: return '{0}({1:d})'.format(self.label, self.index)
 	
+	## need to rewrite later
 	def _repr_png_(self):
 		if len(self.species_repr.molecule) > 0:
 			return self.species_repr.molecule[0]._repr_png_()
