b01be5e7f1f116b879eb9a5c2ab5a52db74828b8
KEHANG <kehanghan@gmail.com>
2017-10-25 14:50:29 -0400

    purely change tabs to spaces

--- afm/fragment.py
+++ afm/fragment.py
@@ -3,39 +3,39 @@ from rmgpy.molecule.graph import Graph, Vertex
 
 class CuttingLabel(Vertex):
 
-    def __init__(self, label=''):
-        Vertex.__init__(self)
-        self.label = label
+	def __init__(self, label=''):
+		Vertex.__init__(self)
+		self.label = label
 
-    def __str__(self):
-        """
-        Return a human-readable string representation of the object.
-        """
-        return str(self.label)
+	def __str__(self):
+		"""
+		Return a human-readable string representation of the object.
+		"""
+		return str(self.label)
 
-    def __repr__(self):
-        """
-        Return a representation that can be used to reconstruct the object.
-        """
-        return "<CuttingLabel '{0}'>".format(str(self))
+	def __repr__(self):
+		"""
+		Return a representation that can be used to reconstruct the object.
+		"""
+		return "<CuttingLabel '{0}'>".format(str(self))
 
-    def isSpecificCaseOf(self, other):
-        """
-        Return ``True`` if `self` is a specific case of `other`, or ``False``
-        otherwise. At this moment, this is the same as the :meth:`equivalent()`.
-        """
-        return self.equivalent(other)
+	def isSpecificCaseOf(self, other):
+		"""
+		Return ``True`` if `self` is a specific case of `other`, or ``False``
+		otherwise. At this moment, this is the same as the :meth:`equivalent()`.
+		"""
+		return self.equivalent(other)
 
-    def equivalent(self, other):
-        """
-        Return ``True`` if `other` is indistinguishable from this CuttingLabel, or
-        ``False`` otherwise. If `other` is an :class:`CuttingLabel` object, then all
-        attributes must match exactly. 
-        """
-        if isinstance(other, CuttingLabel):
-            return self.label == other.label
-        else:
-            return False
+	def equivalent(self, other):
+		"""
+		Return ``True`` if `other` is indistinguishable from this CuttingLabel, or
+		``False`` otherwise. If `other` is an :class:`CuttingLabel` object, then all
+		attributes must match exactly. 
+		"""
+		if isinstance(other, CuttingLabel):
+			return self.label == other.label
+		else:
+			return False
 
 class Fragment(Graph):
 
