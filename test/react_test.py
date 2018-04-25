e80d598966f2ba6d72875c277f8e9f96f2092536
KEHANG <kehanghan@gmail.com>
2018-04-24 22:31:46 -0400

    add a new react_fragments test

--- test/react_test.py
+++ test/react_test.py
@@ -7,8 +7,6 @@ from rmgpy.data.rmg import RMGDatabase
 import afm.react
 import afm.fragment
 
-TESTFAMILY = 'R_Recombination'
-
 class TestReact(unittest.TestCase):
 
     def setUp(self):
@@ -24,7 +22,7 @@ class TestReact(unittest.TestCase):
         self.database.loadForbiddenStructures(os.path.join(db_path, 'forbiddenStructures.py'))
         # kinetics family loading
         self.database.loadKinetics(os.path.join(db_path, 'kinetics'),
-                                   kineticsFamilies=[TESTFAMILY],
+                                   kineticsFamilies='default',
                                    reactionLibraries=[]
                                    )
 
@@ -34,7 +32,19 @@ class TestReact(unittest.TestCase):
         fragment_tuple = (frag1, )
         reactions = afm.react.react_fragments(self.database.kinetics, 
                                               fragment_tuple,
-                                              only_families=[TESTFAMILY],
+                                              only_families=['R_Recombination'],
                                               prod_resonance=False)
 
         self.assertTrue(len(reactions)==28) 
+
+    def test_react_fragments2(self):
+        
+        frag1 = afm.fragment.Fragment(label='frag1').from_SMILES_like_string('c1ccccc1CCCR')
+        frag2 = afm.fragment.Fragment(label='frag2').from_SMILES_like_string('[CH2]CR')
+        fragment_tuple = (frag1, frag2)
+        reactions = afm.react.react_fragments(self.database.kinetics, 
+                                              fragment_tuple,
+                                              only_families=['H_Abstraction'],
+                                              prod_resonance=False)
+
+        self.assertTrue(len(reactions)==11) 
